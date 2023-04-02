886968ae7330338cc50e2e0f80ec286e77c12629
vadymsirenko <vadymsirenko@gmail.com>
2023-04-02 10:38:36 +0200

    ukr tv youtube chanels PWA ver. #1 sw

--- js/app.js
+++ js/app.js
@@ -1,11 +1,37 @@
 window.addEventListener('load', () => {
+    // виклик модального вікна
+    const askUserToUpdate = reg => {
+        return Modal.confirm({
+            onOk: async () => {
+                // оборбляємо зміну стану
+                navigator.serviceWorker.addEventListener('controllerchange', () => {
+                    window.location.reload();
+                });
+
+                // пропускаємо очікування 
+                if (reg && reg.waiting) {
+                    reg.waiting.postMessage({ type: 'SKIP_WAITING' });
+                }
+            },
+
+            onCancel: () => {
+                Modal.destroyAll();
+            },
+            icon: null,
+            title: 'Гарні новини!!!',
+            content:
+                'Программа вдосконалюється! Якщо ви бажаєте оновити її, натисніть кнопку нижче',
+            cancelText: 'Відкласти',
+            okText: 'Оновити'
+        });
+    };
 
     if ('serviceWorker' in navigator) {
 
         navigator.serviceWorker.register('./serviceworker.js')
             .then(registration => {
                 if (registration.waiting) {
-                    // оброботчик SW в ожидании
+                    // обробник SW в очікуванні
                     askUserToUpdate(registration);
                 }
                 console.log('Service worker successfully registered', registration);
